11b9e61b0e4880284a198ed5878b5857ce70559a
Dede Herman <dedehermanvpay@gmail.com>
2020-10-19 13:15:06 +0700

    modified content

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,14 @@ var urlsToCache = [
     "/",
     "/nav.html",
     "/index.html",
+    "/article.html",
     "/pages/home.html",
     "/pages/about.html",
     "/pages/contact.html",
     "/css/materialize.min.css",
     "/js/materialize.min.js",
     "/js/nav.js",
+    "/js/api.js",
     "/icon.png",
     "/manifest.json"
 ];
@@ -22,22 +24,23 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches
-        .match(event.request, { cacheName: CACHE_NAME })
-        .then(function(response) {
-            if (response) {
-                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-                return response;
-            }
-
-            console.log(
-                "ServiceWorker: Memuat aset dari server: ",
-                event.request.url
-            );
-            return fetch(event.request);
-        })
-    );
+    var base_url = "https://readerapi.codepolitan.com/";
+    if (event.request.url.indexOf(base_url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function(cache) {
+                return fetch(event.request).then(function(response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+                return response || fetch(event.request);
+            })
+        )
+    }
 });
 
 self.addEventListener("activate", function(event) {
